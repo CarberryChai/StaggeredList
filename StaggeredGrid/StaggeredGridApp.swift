31f0ed4e8011bc9152e1cb28fd9b01392f7edfd2
Carberry <CarberryChai@gmail.com>
2021-07-16 21:44:26 +0800

    Initial Commit

---
+++ StaggeredGrid/StaggeredGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaggeredGridApp.swift
+//  StaggeredGrid
+//
+//  Created by 柴长林 on 2021/7/16.
+//
+
+import SwiftUI
+
+@main
+struct StaggeredGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
